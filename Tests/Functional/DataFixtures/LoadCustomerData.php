2c35359cc9cb1723a767f213dd0cec61dc18d936
Dmitry Khrysev <x86demon@users.noreply.github.com>
2022-01-24 16:39:31 +0200

    CRM-9332: Mailchimp sync fails if custom entity field is used as email contact information (#31188)
    - made First/Last Name fields of Customer entity not required for Mailchimp integration

---
+++ Tests/Functional/DataFixtures/LoadCustomerData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oro\Bundle\MailChimpBundle\Tests\Functional\DataFixtures;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Persistence\ObjectManager;
+use Oro\Bundle\TestFrameworkCRMBundle\Entity\TestCustomerWithContactInformation;
+
+class LoadCustomerData extends AbstractFixture
+{
+    /**
+     * @var array
+     */
+    protected $data = [
+        [
+            'name' => 'customer1',
+            'email' => 'customer1@example.com',
+            'phone' => '+12345678900',
+        ],
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->data as $data) {
+            $customer = new TestCustomerWithContactInformation();
+
+            $customer->setName($data['name']);
+            $customer->setEmail($data['email']);
+            $customer->setPhone($data['phone']);
+
+            $manager->persist($customer);
+            $this->setReference('mc:customer:' . $data['name'], $customer);
+        }
+
+        $manager->flush();
+    }
+}
--- Tests/Functional/DataFixtures/LoadMarketingListData.php
+++ Tests/Functional/DataFixtures/LoadMarketingListData.php
@@ -4,7 +4,10 @@ namespace Oro\Bundle\MailChimpBundle\Tests\Functional\DataFixtures;
 
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Oro\Bundle\ContactBundle\Entity\Contact;
 use Oro\Bundle\MarketingListBundle\Entity\MarketingList;
+use Oro\Bundle\MarketingListBundle\Entity\MarketingListType;
+use Oro\Bundle\TestFrameworkCRMBundle\Entity\TestCustomerWithContactInformation;
 
 class LoadMarketingListData extends AbstractMailChimpFixture implements DependentFixtureInterface
 {
@@ -16,9 +19,17 @@ class LoadMarketingListData extends AbstractMailChimpFixture implements Dependen
             'type' => 'dynamic',
             'name' => 'Test ML',
             'description' => '',
-            'entity' => 'Oro\Bundle\ContactBundle\Entity\Contact',
+            'entity' => Contact::class,
             'reference' => 'mailchimp:ml_one',
             'segment' => 'mailchimp:ml_one:segment',
+        ],
+        [
+            'type' => 'dynamic',
+            'name' => 'Test ML Customer',
+            'description' => '',
+            'entity' => TestCustomerWithContactInformation::class,
+            'reference' => 'mailchimp:ml_two',
+            'segment' => 'mailchimp:ml_two:segment',
         ],
     ];
 
@@ -30,7 +41,7 @@ class LoadMarketingListData extends AbstractMailChimpFixture implements Dependen
         foreach ($this->mlData as $data) {
             $entity = new MarketingList();
             $type = $manager
-                ->getRepository('OroMarketingListBundle:MarketingListType')
+                ->getRepository(MarketingListType::class)
                 ->find($data['type']);
             $segment = $this->getReference($data['segment']);
             $entity->setType($type);
@@ -50,6 +61,7 @@ class LoadMarketingListData extends AbstractMailChimpFixture implements Dependen
         return [
             LoadSegmentData::class,
             LoadContactData::class,
+            LoadCustomerData::class,
         ];
     }
 }
