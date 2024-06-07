dfdd55c680add2b2a493f731d9f343e0665102ab
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2024-06-07 16:25:32 +0200

    CRM-9421: MailChimp integrations incorrectly handles missing MemberActivity fields on import (#38626)

--- ImportExport/DataConverter/MemberActivityDataConverter.php
+++ ImportExport/DataConverter/MemberActivityDataConverter.php
@@ -19,14 +19,25 @@ class MemberActivityDataConverter extends IntegrationAwareDataConverter
     }
 
     /**
+     * Please note that if there is no activity data, the activity entity will be ignored and not saved.
+     *
      * {@inheritdoc}
      */
     public function convertToImportFormat(array $importedRecord, $skipNullValues = true)
     {
-        $importedRecord['member:email'] = $importedRecord['email'] ?? $importedRecord['email_address'];
-        $importedRecord['action'] = $importedRecord['activity']['action'] ?? $importedRecord['action'];
-        $importedRecord['activityTime'] = $importedRecord['activity']['timestamp'] ?? $importedRecord['activityTime'];
-        $importedRecord['ip'] = $importedRecord['activity']['ip'] ?? $importedRecord['ip'];
+        $importedRecord['member:email'] = $importedRecord['email_address'];
+
+        if (isset($importedRecord['activity']['action'])) {
+            $importedRecord['action'] = $importedRecord['activity']['action'];
+        }
+
+        if (isset($importedRecord['activity']['timestamp'])) {
+            $importedRecord['activityTime'] = $importedRecord['activity']['timestamp'];
+        }
+
+        if (isset($importedRecord['activity']['ip'])) {
+            $importedRecord['ip'] = $importedRecord['activity']['ip'];
+        }
 
         return parent::convertToImportFormat($importedRecord, $skipNullValues);
     }
